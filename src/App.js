d5c1102952ca7b076bbc3aca958ea2f80d1c03d9
kurniawan26 <71652809+kurniawan26@users.noreply.github.com>
2020-09-27 23:15:12 +0700

    penambahan HTML & Styling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,54 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-btn">Add</button>
+          <h1 className="header-title">To Do List</h1>
+          <button className="header-btn primary">Clear</button>
+        </section>
+
+        <section className="add">
+          <form className="add-form">
+            <input type="text" className="add-input" />
+            <button className="add-btn thirdty">Add</button>
+          </form>
+        </section>
+
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
